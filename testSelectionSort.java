e4f415d33fb932ecd9c583414a3d28000089a0ea
John Jasinski <johnjazz@umich.edu>
2020-12-03 18:43:42 -0500

    Set up tests for positive and negative

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -12,79 +12,80 @@ class testSelectionSort {
 		testMixed();
 		testDuplicates();
 	}
-	
+
 	public testSelectionSort() {}
-	
-	public void testPositive() { 
-	int[] arr = new int[5];        
-	arr[0] = 8;       
-	arr[1] = 9;        
-	arr[2] = 7;       
-	arr[3] = 10;        
+
+	public void testPositive() {
+	int[] arr = new int[5];
+	arr[0] = 8;
+	arr[1] = 9;
+	arr[2] = 7;
+	arr[3] = 10;
 	arr[4] = 2;
-	
+
 	int[] sortedArr = new int[5];
 	sortedArr[0] = 2;
 	sortedArr[1] = 7;
 	sortedArr[2] = 8;
 	sortedArr[3] = 9;
 	sortedArr[4] = 10;
-	
-	
+
+
+
 	}
-	
+
 	public void testNegative() {
-		int[] arr = new int[5];        
-		arr[0] = -8;       
-		arr[1] = -9;        
-		arr[2] = -7;       
-		arr[3] = -10;        
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
 		arr[4] = -2;
-		
+
 		int[] sortedArr = new int[5];
 		sortedArr[4] = -2;
 		sortedArr[3] = -7;
 		sortedArr[2] = -8;
 		sortedArr[1] = -9;
 		sortedArr[0] = -10;
-		
-		
+
+
 	}
-	
+
 	public void testMixed() {
-		int[] arr = new int[5];        
-		arr[0] = 2;       
-		arr[1] = -7;        
-		arr[2] = 8;       
-		arr[3] = -9;        
+		int[] arr = new int[5];
+		arr[0] = 2;
+		arr[1] = -7;
+		arr[2] = 8;
+		arr[3] = -9;
 		arr[4] = 10;
-		
+
 		int[] sortedArr = new int[5];
 		sortedArr[0] = -9;
 		sortedArr[1] = -7;
 		sortedArr[2] = 2;
 		sortedArr[3] = 8;
 		sortedArr[4] = 10;
-		
+
 	}
-	
+
 	public void testDuplicates() {
-		int[] arr = new int[5];        
-		arr[0] = 2;       
-		arr[1] = 2 ;      
-		arr[2] = -9 ;     
-		arr[3] =  7  ;    
+		int[] arr = new int[5];
+		arr[0] = 2;
+		arr[1] = 2 ;
+		arr[2] = -9 ;
+		arr[3] =  7  ;
 		arr[4] = -9;
-		
+
 		int[] sortedArr = new int[5];
 		sortedArr[0] = -9;
 		sortedArr[1] = -9;
 		sortedArr[2] = 2;
 		sortedArr[3] = 2;
 		sortedArr[4] = 7;
-		
-		
+
+
 	}
-	
+
 
 }
